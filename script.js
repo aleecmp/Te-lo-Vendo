ceb7f7246645c0e6f4a45bab265da44550f53a07
aleecmp <ale.cmpb@gmail.com>
2023-03-22 12:00:30 -0300

    feat: add functionality to the hamburger button

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    let menu = document.getElementsByClassName('main-nav-list')[0];
-    console.log(menu)
-    menu.classList.toggle('nav-open');
-
-  }
---
+++ script/script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+  const menu = document.getElementsByClassName('main-nav-list')[0];
+  menu.classList.toggle('nav-open');
+}
